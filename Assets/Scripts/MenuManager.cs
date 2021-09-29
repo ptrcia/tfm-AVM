71c1269c117638f3157db8bf3dda50a55a24e700
ptrcia <patryechelon@gmail.com>
2021-09-29 20:19:31 +0200

    Nuevas escenas y transporte entre ellas
    -escena menu con boton de empezar
    -escena1 con la puerta básica que teletransporta
    -nuevos modelos
    -nuevos scripts

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+public void PlayGame()
+    {
+        SceneManager.LoadScene("1escena");
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
