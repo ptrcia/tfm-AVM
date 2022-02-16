8c46d5e23f272e468ba78835c211b4f2ec3399b7
ptrcia <patryechelon@gmail.com>
2022-02-16 15:29:23 +0100

    Actualizado el controlador de las escenas

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -10,9 +10,41 @@ public class MenuManager : MonoBehaviour
         if (Input.GetKey("escape"))
         {
             Debug.Log("Quit");
-
             Application.Quit();
         }
+        if (Input.GetKey("0"))
+        {
+            SceneManager.LoadScene("0-menu");
+        }
+        if (Input.GetKey("1"))
+        {
+            SceneManager.LoadScene("1-casa");
+        }
+        if (Input.GetKey("2"))
+        {
+            SceneManager.LoadScene("2-fiesta");
+        }
+        if (Input.GetKey("3"))
+        {
+            SceneManager.LoadScene("3-pasillo");
+        }
+        if (Input.GetKey("4"))
+        {
+            SceneManager.LoadScene("4-camas");
+        }
+        if (Input.GetKey("5"))
+        {
+            SceneManager.LoadScene("5-espejos");
+        }
+        if (Input.GetKey("6"))
+        {
+            SceneManager.LoadScene("6-escalera");
+        }
+    }
+
+    public void MenuGame()
+    {
+        SceneManager.LoadScene("0-menu");
     }
     public void PlayGame()
     {
@@ -38,13 +70,6 @@ public class MenuManager : MonoBehaviour
     {
         SceneManager.LoadScene("6-escalera");
     }
-
-
-    public void MenuGame()
-    {
-        SceneManager.LoadScene("0-menu");
-    }
-
     public void QuitGame()
     {
         Debug.Log("Quit");
